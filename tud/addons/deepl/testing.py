fae0095c3afbf7df91d8976eb4db4ce88c0308c3
Paul Grunewald <paul.grunewald@tu-dresden.de>
2023-09-07 01:15:43 +0200

    WEBCMS-4515: Refactor setup_deepl_from_environment
    It can be easily reused by every testing layer that needs the
    API key from the environment.

--- tud/addons/deepl/testing.py
+++ tud/addons/deepl/testing.py
@@ -1,5 +1,4 @@
 import os
-import transaction
 
 from plone import api
 from plone.app.robotframework.testing import REMOTE_LIBRARY_BUNDLE_FIXTURE
@@ -9,8 +8,14 @@ from plone.app.testing import IntegrationTesting
 from plone.app.testing import PloneSandboxLayer
 from plone.app.testing import applyProfile
 from plone.testing import z2
-
 import tud.addons.deepl
+
+
+def setup_deepl_from_environment():
+    """Setup Deepl key from environment
+    """
+    token = os.getenv("DEEPL_API_TOKEN", "")
+    api.portal.set_registry_record("tud.addons.deepl.interfaces.IDeepLAPISettings.deepl_api_auth_token", unicode(token))
 
 
 class TudAddonsDeeplLayer(PloneSandboxLayer):
@@ -35,11 +40,7 @@ class TudAddonsDeeplLayer(PloneSandboxLayer):
 
     def setUpPloneSite(self, portal):
         applyProfile(portal, "tud.addons.deepl:test")
-        token = os.getenv("DEEPL_API_TOKEN", "")
-        api.portal.set_registry_record("tud.addons.deepl.interfaces.IDeepLAPISettings.deepl_api_auth_token", unicode(token))
-
-        transaction.commit()
-
+        setup_deepl_from_environment()
 
 FIXTURE = TudAddonsDeeplLayer()
 
