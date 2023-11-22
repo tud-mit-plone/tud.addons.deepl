28d0a422eabcb91905b7085cae8e1cb06b002e5c
Peter Seifert <peter.seifert1@tu-dresden.de>
2023-11-22 19:16:27 +0100

    WEBCMS-4568 move deepl test registry setup into test profile setuphandler

--- tud/addons/deepl/testing.py
+++ tud/addons/deepl/testing.py
@@ -1,6 +1,3 @@
-import os
-
-from plone import api
 from plone.app.robotframework.testing import REMOTE_LIBRARY_BUNDLE_FIXTURE
 from plone.app.testing import PLONE_FIXTURE
 from plone.app.testing import FunctionalTesting
@@ -8,14 +5,6 @@ from plone.app.testing import IntegrationTesting
 from plone.app.testing import PloneSandboxLayer
 from plone.testing.zope import WSGI_SERVER_FIXTURE
 from plone.testing.zope import installProduct
-import tud.addons.deepl
-
-
-def setup_deepl_from_environment():
-    """Setup Deepl key from environment
-    """
-    token = os.getenv("DEEPL_API_TOKEN", "")
-    api.portal.set_registry_record("tud.addons.deepl.interfaces.IDeepLAPISettings.deepl_api_auth_token", unicode(token))
 
 
 class TudAddonsDeeplLayer(PloneSandboxLayer):
@@ -26,21 +15,16 @@ class TudAddonsDeeplLayer(PloneSandboxLayer):
         # call super setUpZope
         super(TudAddonsDeeplLayer, self).setUpZope(app, configurationContext)
 
-        # Load any other ZCML that is required for your tests.
-        # The z3c.autoinclude feature is disabled in the Plone fixture base
-        # layer.
-        # import plone.app.dexterity
-        # self.loadZCML(package=plone.app.dexterity)
         import plone.rest
+        import tud.addons.deepl
 
         self.loadZCML(package=plone.rest)
-
         self.loadZCML(name="testing.zcml", package=tud.addons.deepl)
         installProduct(app, "tud.addons.deepl")
 
     def setUpPloneSite(self, portal):
         self.applyProfile(portal, "tud.addons.deepl:test")
-        setup_deepl_from_environment()
+
 
 FIXTURE = TudAddonsDeeplLayer()
 
